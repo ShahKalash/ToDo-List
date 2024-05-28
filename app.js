016e214f00bf6d669e8f3168cd6ee6f140a8f158
Kalash <kalash.s@ahduni.edu.in>
2024-05-28 22:56:13 +0530

    generalize routes for any passed params

--- app.js
+++ app.js
@@ -19,45 +19,91 @@ const Item1 = new Item({
 });
 
 // Item.insertMany([Item1]);
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
 
-
+const list = mongoose.model("List", listSchema);
 app.get('/', (req, res) => {
-    var data = [];
-    async function findItem() {
-        data = await Item.find();
-        if (data.length === 0) {
-            data = [{ name: "Enter tasks to your todo List!!" }];
-        }
-        res.render('index', { data: data });
-    }
-    findItem();
+    res.redirect('/home');
+});
+
+app.get("/:customListName", (req, res) => {
+    const customListName = req.params.customListName;
+    list.findOne({ name: customListName })
+        .then((docs) => {
+            if (!docs) {
+                const list1 = new list({
+                    name: customListName,
+                    items: [{ name: "Enter tasks to your todo List" }]
+                })
+                list.insertMany([list1]);
+                res.redirect('/' + customListName);
+            }
+            else {
+                res.render('index', { data: docs.items, title: customListName });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+
 });
 app.post('/', (req, res) => {
     var item = req.body.item;
-    console.log(item);
+    var listName = req.body.title;
+    console.log(listName);
     const Item2 = new Item({
         name: item
     });
-    Item.insertMany([Item2]);
-    res.redirect('/');
+    list.findOne({ name: listName })
+        .then((docs) => {
+            docs.items.push(Item2);
+            docs.save();
+            res.redirect('/' + listName);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
-app.post('/delete', (req, res) => {
+app.post('/delete', async (req, res) => {
     console.log(req.body);
-    Item.deleteOne({ _id: req.body.tobedeleted }).then(function () {
-        console.log("Data deleted"); // Success
-        res.redirect('/');
-    }).catch(function (error) {
-        console.log(error); // Failure
-    });
+    const listName = req.body.title;
+    try {
+        await list.findOneAndUpdate(
+            { name: listName },
+            { $pull: { items: { _id: req.body.tobedeleted } } }
+        ).exec();
+
+        res.redirect('/' + listName);
+    } catch (error) {
+        console.error("Error deleting item:", error);
+        res.status(500).send("Error deleting item.");
+    }
 });
 
-app.post('/search', (req, res) => {
-    var data = [];
-    async function findItem() {
-        data = await Item.find({ name: req.body.search });
-        res.render('index', { data: data });
+app.post('/search', async (req, res) => {
+    const search = req.body.search;
+    const listName = req.body.title;
+    try {
+        const list1 = await list.findOne({ name: listName });
+
+        if (!list1) {
+            // Handle case where list doesn't exist
+            return res.status(404).send("List not found");
+        }
+
+        // Use filter to find items matching the search value
+        const matchedItems = list1.items.filter(item => item.name === search);
+
+        // Send the matched items to the client
+        res.render('index', { title: listName, data: matchedItems });
+    } catch (error) {
+        console.error("Error finding items:", error);
+        res.status(500).send("Error finding items.");
     }
-    findItem();
 });
 app.listen(3000, () => {
     console.log('listening on port 3000');
